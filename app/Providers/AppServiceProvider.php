4ebcdc4c139f101201d654fcdd3a55eca7a2a843
Cristian Ionel <cristian.ionel@gmail.com>
2019-06-13 00:14:45 +0200

    Key too short

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Blade::component('components.referrals', 'referrals');
         Blade::component('components.ambassadors', 'ambassadors');
     }
